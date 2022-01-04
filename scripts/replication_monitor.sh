3dd22a9aaa4c45f05b194a16479488bf3ed7b6b9
IulianEles <eiulian21@gmail.com>
2022-01-04 18:48:38 +0200

    Update replication_monitor.sh
    fixed password issue

--- scripts/replication_monitor.sh
+++ scripts/replication_monitor.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
-
-password=`</dev/urandom tr -dc '123456789!@#$%qwe^CE' | head -c8`
+password=$(openssl rand -base64 12 )
 user=check_monit
 default_password=$3
 default_user=$2
-sender=mysql-replication@layershift.com
+sender=mysql-replication@$(hostname)
 email=monitor@layershift.com
 credentials=/opt/ls_tools/.sqlpwd
 tools=/opt/ls_tools
 server=$(hostname)
-if [ "$1" = "--help" ]
-    then
+disable=/opt/ls_tools/disabled.txt
+if [ "$1" = "--help" ] ; then
     echo "This is the help section of the script:"
     echo "For the first run, you need to create the sql user. To do so execute :"
     echo "sh /opt/ls_tools/replication_monitor.sh --create mysql_user mysql_password"
@@ -18,15 +17,9 @@ if [ "$1" = "--help" ]
     echo ""
     echo "After the user was created, in order to run the checks, execute the script with the --check option"
 fi
-if [ ! -d $tools ]
-    then
-        mkdir $tools
-fi
+if [ ! -d $tools ]; then mkdir $tools; fi
 
-if [ ! -f $credentials ]
-    then
-        touch $credentials
-fi
+if [ ! -f $credentials ] ; then touch $credentials; fi
 
 ###Create user
 function create_user () {
@@ -45,28 +38,25 @@ function enable_mail () {
 
 ###Check replication
 function check_replication () {
-local mysql="/usr/bin/mysql --defaults-extra-file=$credentials"
-##### Get Slave Status #####
-mysql_status=`echo "show slave status\G" | $mysql | sed -e 's/^[[:space:]]*//g' 2>&1`
+    local mysql="/usr/bin/mysql --defaults-extra-file=$credentials"
 
-##### Check Replication Delay #####
-seconds_behind_master=`echo "$mysql_status" | grep "Seconds_Behind_Master" | awk '{ print $2 }'`
+    ##### Get Slave Status #####
+    mysql_status=`echo "show slave status\G" | $mysql | sed -e 's/^[[:space:]]*//g' 2>&1`
 
-##### Check if IO thread is running #####
-io_is_running=`echo "$mysql_status" | grep "Slave_IO_Running" | awk '{ print $2 }'`
+    ##### Check Replication Delay #####
+    seconds_behind_master=`echo "$mysql_status" | grep "Seconds_Behind_Master" | awk '{ print $2 }'`
 
-if [ "$io_is_running" != "Yes" ]
- then
-    echo "Mysql replication broken on $server. " | /bin/mail  -s "Mysql replication broken on $server" $email
-fi
+    ##### Check if IO thread is running #####
+    io_is_running=`echo "$mysql_status" | grep "Slave_IO_Running" | awk '{ print $2 }'`
+
+    if [ "$io_is_running" != "Yes" ]; then echo "Mysql replication broken on $server. " | /bin/mail  -s "Mysql replication broken on $server" $email; fi
 }
 
-if [ "$1" == "--create" ]
-    then
-    create_user
-    enable_mail
+function disable_check () {
+    if [[ $(find "$disable" -mtime +1 -print) ]]; then
+  echo "File $filename exists and is older than 100 days"
 fi
-if [ "$1" == "--check" ]
-    then
-    check_replication
-fi
+
+}
+if [ "$1" == "--create" ]; then  create_user; enable_mail; fi
+if [ "$1" == "--check" ]; then check_replication; fi
