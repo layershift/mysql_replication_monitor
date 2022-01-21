2efbb1a5aec0675f28a5c5a1751f5fb0d34f12a3
IulianEles <eiulian21@gmail.com>
2022-01-21 11:55:48 +0200

    added the option to disable the check

--- scripts/replication_monitor.sh
+++ scripts/replication_monitor.sh
@@ -8,7 +8,9 @@ email=monitor@layershift.com
 credentials=/opt/ls_tools/.sqlpwd
 tools=/opt/ls_tools
 server=$(hostname)
-disable=/opt/ls_tools/disabled.txt
+time=$2
+config_file=/opt/ls_tools/mysql_monitor.config
+
 if [ "$1" = "--help" ] ; then
     echo "This is the help section of the script:"
     echo "For the first run, you need to create the sql user. To do so execute :"
@@ -16,11 +18,18 @@ if [ "$1" = "--help" ] ; then
     echo "and replace mysql_user and mysql_password with the correct values"
     echo ""
     echo "After the user was created, in order to run the checks, execute the script with the --check option"
+    echo "If you want to disable the check for a period, you can use --check --time=n (where n is any time in hours you want it disabled)"
 fi
 if [ ! -d $tools ]; then mkdir $tools; fi
 
 if [ ! -f $credentials ] ; then touch $credentials; fi
 
+###Populate config file
+function add_data () {
+    if [ ! -f $config_file ] ; then touch $config_file; fi
+    echo "mysql_status= " > $config_file
+    echo "disabled_status= " > $config_file
+}
 ###Create user
 function create_user () {
     echo "CREATE USER '$user'@'localhost' IDENTIFIED BY '$password';" | mysql -u$default_user -p$default_password
@@ -35,37 +44,51 @@ function enable_mail () {
     /bin/systemctl start sendmail
     /bin/yum install mailx -y
 }
+###Disable checks for e period
+function check_disable () {
+    disable_check_file=/opt/ls_tools/disabled.txt
+    lastUpdate=$(date -r $disable_check_file +%s)
+    now=$(date +%s)
+    file_age_seconds=$((now - lastUpdate))
+    file_age=$(((now - lastUpdate)/3600))
+    time="${time#*=}"
+    disable_time=`cat $disable_check_file`
+    if [ ! -f $disable_check_file ]; then touch $disable_check_file; fi
+    if [ "$disable_time" -eq "0" ]; then echo "$time" > $disable_check_file ; fi
+    disable_time=`cat $disable_check_file`
+    echo -e "file age is $file_age and the settings are $disable_time"
+    if [ "$disable_time" -gt "$file_age" ]; then sed -i 's/disabled_status=/disabled_status= true/g' $config_file ; else sed -i 's/disabled_status=/disabled_status= false/g' $config_file && echo "0" > $disable_check_file  ; fi
+}
+
+###Check if mysql connection works with monitoring user
+function check_mysql () {
+    local mysql="/usr/bin/mysql --defaults-extra-file=$credentials"
+    echo "show databases;" | $mysql
+    if [ "$?" == "0" ]; then 's/mysql_status=/mysql_status= true/g' $config_file ; else 's/mysql_status=/mysql_status= false/g' $config_file ; fi
+}
 
 ###Check replication
 function check_replication () {
     local mysql="/usr/bin/mysql --defaults-extra-file=$credentials"
-
-    ##### Get Slave Status #####
-    mysql_status=`echo "show slave status\G" | $mysql | sed -e 's/^[[:space:]]*//g' 2>&1`
-
-    ##### Check Replication Delay #####
-    seconds_behind_master=`echo "$mysql_status" | grep "Seconds_Behind_Master" | awk '{ print $2 }'`
-
+    local disabled_status=`grep "disabled_status=" $config_file  |awk '{ print $2 }'`
+    local mysql_status=`grep "mysql_status=" $config_file  |awk '{ print $2 }'`
+    local io_is_running=`echo "$mysql_status" | grep "Slave_IO_Running" | awk '{ print $2 }'`
     ##### Check if IO thread is running #####
-    io_is_running=`echo "$mysql_status" | grep "Slave_IO_Running" | awk '{ print $2 }'`
-
+    if [ "$disabled_status" != "false" ]; then exit 0 ; fi
+    if [ "$mysql_status" != "true" ]; then echo "Mysql connection broken on $server. " | /bin/mail  -s "Mysql connection broken on $server" $email; exit 0; fi   
     if [ "$io_is_running" != "Yes" ]; then echo "Mysql replication broken on $server. " | /bin/mail  -s "Mysql replication broken on $server" $email; fi
 }
-
-function disable_check () {
-    if [[ $(find "$disable" -mtime +1 -print) ]]; then
-  echo "File $filename exists and is older than 100 days"
-fi
-
-}
-
 
 ###Uninstall
 function uninstall () {
     /usr/bin/mysql -u$default_user -p$default_password -e "DROP USER 'check_monit'@'localhost';"
     /usr/bin/rm -f $credentials
+    /usr/bin/rm -f $config_file
+    /usr/bin/rm -f $disable_check_file
+
 }
 
 if [ "$1" == "--create" ]; then  create_user; enable_mail; fi
-if [ "$1" == "--check" ]; then check_replication; fi
+if [ "$1" == "--disable" ]; then check_disable ; fi
+if [ "$1" == "--check" ]; then check_disable; check mysql; check_replication; fi
 if [ "$1" == "--uninstall" ]; then uninstall; fi
