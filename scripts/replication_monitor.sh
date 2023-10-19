3083e93b18a65e0d08278665035ef4aafa36bc9f
Raul Adorean <raul@layershift.me>
2023-10-19 14:15:48 +0000

    Bugfix: handle missing status_file issue #6

--- scripts/replication_monitor.sh
+++ scripts/replication_monitor.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+version=2023101901
+
 server=$(hostname)
 sender=mysql-replication@${server}
 tools=/opt/ls_tools
@@ -11,7 +14,25 @@ debug=0
 if [ ! -d $tools ]; then mkdir $tools; fi
 if [ ! -f $credentials ] ; then touch $credentials; fi
 
-###Populate config file with mysql_status and disabled_status
+### Check status_file
+function check_status_file () {
+    if [ ! -f $status_file ] ; then 
+        touch $status_file;
+        echo -e "mysql_status= " >> $status_file
+        echo -e "disabled_status= " >> $status_file
+    else
+        grep -v "#" $status_file | grep -q "mysql_status="
+        if [ $? -ne 0 ]; then
+            echo -e "mysql_status= " >> $status_file
+        fi
+        grep -v "#" $status_file | grep -q "disabled_status="
+        if [ $? -ne 0 ]; then
+            echo -e "disabled_status= " >> $status_file
+        fi
+    fi
+}
+
+### Populate config file with mysql_status and disabled_status
 function add_data () {
     if [ ! -f $config_file ] ; then 
         touch $config_file; 
@@ -19,11 +40,10 @@ function add_data () {
     else
         sed "s#email=.*#email=${setup_email}#" -i $config_file
     fi
-    if [ ! -f $status_file ] ; then touch $status_file; fi
-    echo -e "mysql_status= " >> $status_file
-    echo -e "disabled_status= " >> $status_file
+    check_status_file
 }
-###Create user with credentials provided in Jps package
+
+### Create user with credentials provided in Jps package
 function create_user () {
     local password=${setup_script_mysql_password}
     local user=check_monit
@@ -35,49 +55,65 @@ function create_user () {
     chmod 600 $credentials
 }
 
-###Enable sendmail and mailx
+### Enable sendmail and mailx
 function enable_mail () {
     /bin/systemctl enable sendmail
     /bin/systemctl start sendmail
     /bin/yum install mailx -y 2>&1 >/dev/null
 }
-###Disable checks for e period; used with --disable --time=n (where n is any number of hours you want to disable the check)
+### Disable checks for e period; used with --disable --time=n (where n is any number of hours you want to disable the check)
 function check_disable () {
     disable_check_file=/${tools}/disabled.txt
     if [ ! -f $disable_check_file ]; then touch $disable_check_file; fi
     ###last time the disabled.txt file was updated
     lastUpdate=$(date -r $disable_check_file +%s)
+
     ###Current time
     now=$(date +%s)
+
     ###disabled.txt file age in seconds/hours
     file_age_seconds=$((now - lastUpdate))
     file_age=$(((now - lastUpdate)/3600))
+
     ###Time variabile previously configured
     time="${time#*=}"
+
     ###If time is configured, add it to the config file
     disable_time=`cat $disable_check_file`
-        if [ ! -z "$time" ]; then
-            if [ "$disable_time" -eq "0" ]; then echo "$time" > $disable_check_file ; fi 
-        fi
+    if [ ! -z "$time" ]; then
+        if [ "$disable_time" -eq "0" ]; then 
+            echo "$time" > $disable_check_file ; 
+        fi 
+    fi
     disable_time=`cat $disable_check_file`
     if [ ${debug} -gt 0 ]; then
         echo -e "file age is $file_age and disable time is $disable_time"
     fi
+
+    ### Check $status_file exists
+    check_status_file
+
     ###set disable_status to true/false based on previous values
     if [ "$disable_time" -gt "$file_age" ]; then sed -i 's/disabled_status=.*/disabled_status= true/g' $status_file ; else sed -i 's/disabled_status=.*/disabled_status= false/g' $status_file && echo "0" > $disable_check_file  ; fi
 }
 
-###Check if mysql connection works with monitoring user
+### Check if mysql connection works with monitoring user
 function check_mysql () {
     
     /usr/bin/mysqladmin --defaults-extra-file=$credentials ping 2>&1 >/dev/null
+
+    ### Check $status_file exists
+    check_status_file
+
     ###If can't connect to mysql using the credentials in the /opt/ls_tools/.sqlpwd file email support 
     if [ $? -eq 0 ]; then sed -i 's/mysql_status=.*/mysql_status= true/g' $status_file ; else sed -i 's/mysql_status=.*/mysql_status= false/g' $status_file && echo "0" > $disable_check_file  ; fi
 }
 
-###Check replication
+### Check replication
 function check_replication () {
 
+    ### Check $status_file exists
+    check_status_file
 
     local mysql="/usr/bin/mysql --defaults-extra-file=$credentials"
 
@@ -102,7 +138,7 @@ function check_replication () {
     if [ "${io_is_running,,}" != "yes" ]; then echo -e "Mysql replication broken on $server.\n\nshow slave status\G\n$(echo "show slave status\G"|$mysql)" | if [ ${debug} -gt 0 ]; then less; else /bin/mail  -s "Mysql replication broken on $server" $email; fi; fi
 }
 
-###show slave status
+### show slave status
 function show_status () {
     local mysql="/usr/bin/mysql --defaults-extra-file=$credentials"
     echo -e "\nOn $server.\n";
@@ -114,7 +150,7 @@ function show_status () {
     exit 0;
 }
 
-###Uninstall
+### Uninstall
 function uninstall () {
     /usr/bin/mysql -u${setup_user} -p${setup_password} -e "DROP USER IF EXISTS 'check_monit'@'localhost';"
     /usr/bin/rm -f $credentials
@@ -132,6 +168,9 @@ while [[ $# -gt 0 ]]; do
 This script is designed to monitor and email the configured "Alert recipient(s)" if the database replication fails
 
 Before the replication can be monitored we need to set up a monitoring database user using --create
+
+Version: $version
+
     sh $0 --create 'mysql_user' 'mysql_password' 'recipient_email' 'script_mysql_password'
 
     sh $0 --check                                       Check the slave status and email if any issues
